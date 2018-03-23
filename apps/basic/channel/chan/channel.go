eab05c8144a1ef1744a0ab77b19912fafa360c49
elite_jigang <elite_jigang@163.com>
2018-03-23 13:43:14 +0800

    added

---
+++ apps/basic/channel/chan/channel.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+	"log"
+)
+
+func asChan(vs ...int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for _, v := range vs {
+			c <- v
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		}
+		close(c)
+	}()
+	return c
+}
+
+func merge1(a, b <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		adone, bdone := false, false
+		for !adone || !bdone {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					log.Println("a is done")
+					adone = true
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok {
+					log.Println("b is done")
+					bdone = true
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
+}
+
+
+func merge(a, b <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		for a != nil || b != nil {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					fmt.Println("a is done")
+					a = nil
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok {
+					fmt.Println("b is done")
+					b = nil
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
+}
+
+func main() {
+	a := asChan(1, 3, 5, 7)
+	b := asChan(2, 4, 6, 8)
+	//c := merge1(a, b)
+	c := merge(a, b)
+	for v := range c {
+		fmt.Println(v)
+	}
+}
